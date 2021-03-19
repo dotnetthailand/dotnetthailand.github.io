f41f3a9f84ec76a07339ff8147df8fc2472265d1
aaronamm <theeranitp@gmail.com>
2021-03-19 23:07:35 +0700

    Finish simple responsive table with flexbox and horizontal scroll

--- src/components/MdxComponents/ReportingComparisonTable.jsx
+++ src/components/MdxComponents/ReportingComparisonTable.jsx
@@ -1,37 +1,46 @@
 import React from 'react';
 import report from '../../../content/data/reporting.yml'
+import styled from '@emotion/styled';
+
+
+const Wrapper = styled.div`
+  overflow-x: auto;
+  width: 100%;
+  `;
 
 const ReportingComparisonTable = () => {
 
   return (
-    <table className='comparison-table'>
-      <thead>
-        <tr>
-          <th>Report name</th>
-          <th>URL</th>
-          <th>Export PDF</th>
-          <th>Free</th>
-          <th>Open source</th>
-          <th>Report designer tool</th>
-        </tr>
-      </thead>
-      <tbody>
-        {report.reports.map((report, index) =>
-          <tr key={`report_index_${index}`}>
-            <td data-column='Report name' className='_no-wrap'>{report.name}</td>
-            <td data-column='URL' className='_center'>
-              <a href={report.url} >link</a>
-            </td>
-            <td data-column='Export PDF' className='_center'>
-              {report.exportPDF ? 'yes' : 'no'}
-              </td>
-            <td data-column='Free' className='_center'>{report.free ? 'yes' : 'no'}</td>
-            <td data-column='Open source' className='_center'>{report.openSource ? 'yes' : 'no'}</td>
-            <td data-column='Report designer tool'>{report.designerTool}</td>
+    <Wrapper>
+      <table className='comparison-table'>
+        <thead>
+          <tr>
+            <th>Report name</th>
+            <th>URL</th>
+            <th>Export PDF</th>
+            <th>Free</th>
+            <th>Open source</th>
+            <th>Report designer tool</th>
           </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {report.reports.map((report, index) =>
+            <tr key={`report_index_${index}`}>
+              <td data-column='Report name' className='_no-wrap'>{report.name}</td>
+              <td data-column='URL' className='_center'>
+                <a href={report.url} >link</a>
+              </td>
+              <td data-column='Export PDF' className='_center'>
+                {report.exportPDF ? 'yes' : 'no'}
+              </td>
+              <td data-column='Free' className='_center'>{report.free ? 'yes' : 'no'}</td>
+              <td data-column='Open source' className='_center'>{report.openSource ? 'yes' : 'no'}</td>
+              <td data-column='Report designer tool'>{report.designerTool}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </Wrapper>
   )
 };
 
