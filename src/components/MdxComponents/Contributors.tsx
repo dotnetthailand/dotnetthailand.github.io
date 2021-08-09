c683399a0ec999104dc9a9581dd677cc124afe4b
Thada Wangthammang <thada.wth@gmail.com>
2021-08-09 15:08:42 +0700

    feat: list all commit in contributors page

--- src/components/MdxComponents/Contributors.tsx
+++ src/components/MdxComponents/Contributors.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import { useTheme } from 'emotion-theming';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+const axiosConfig = {
+  headers: {
+    'Accept': 'application/vnd.github.v3+json'
+  }
+}
 
 const style = theme => css`
   display: flex;
@@ -33,9 +41,15 @@ const style = theme => css`
   }
 `;
 
+
+const query = typeof window !== 'undefined' ? window.location.search.replace(/^\?/, '') : '';
+
 const Contributors = () => {
 
+  const [commitList, setCommitList] = useState([]);
   const theme = useTheme();
+  var searchParams = new URLSearchParams(query);
+
 
   const { allContributor } = useStaticQuery(graphql`
     query queryContributors {
@@ -51,13 +65,42 @@ const Contributors = () => {
     }
   `);
 
+
   const contributors = allContributor.nodes;
+
+  if (searchParams.has('author')) {
+    const username = searchParams.get('author');
+    console.log('Rerender');
+
+    useEffect(()=> {
+      const fetch = async () => {
+        const data = (await axios.get(`https://api.github.com/repos/dotnetthailand/dotnetthailand.github.io/commits?author=${username}`, axiosConfig)).data;
+        const commitListTmp = data.map(commit => ({
+          date: commit?.commit?.author?.date,
+          message: commit?.commit?.message,
+          url: commit?.url,
+        }))
+        setCommitList(commitListTmp);
+      }
+  
+      fetch();
+    }, []);
+
+
+    return (
+      <div>
+        {commitList.map(commit => (
+          <div key={commit.url}>{commit.date}, {commit.message}</div>
+        ))}
+      </div>
+    )
+  }
   return (
     <div css={style(theme)}>
-      { contributors.map(contributor => {
+      {contributors.map(contributor => {
         return (
           // eslint-disable-next-line react/jsx-no-target-blank
-          <a key={contributor.id} href={contributor.html_url} target='_blank'>
+          <a key={contributor.id} href={`?author=${contributor.login}`} >
             <img
               alt={contributor.login}
               src={contributor.avatar_url}
