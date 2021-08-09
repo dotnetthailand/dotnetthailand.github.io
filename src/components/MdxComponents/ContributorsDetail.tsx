327e21428cae2cb198f40ff8b6f912586e9c5296
Thada Wangthammang <thada.wth@gmail.com>
2021-08-09 18:37:14 +0700

    feat: render only first file of author

--- src/components/MdxComponents/ContributorsDetail.tsx
+++ src/components/MdxComponents/ContributorsDetail.tsx
@@ -8,19 +8,81 @@ const axiosConfig = {
   }
 }
 
+interface ICommit {
+  date: Date;
+  message: string;
+  url: string;
+}
+
+interface IFile {
+  commitCount: number;
+  filename: string;
+}
+
+const setTimeoutPromise = (timeout: number) => new Promise(resolve => {        
+  setTimeout(resolve, timeout);
+});
+
+
+const fetchRetry = async (url: string, delayTime: number, limit: number): Promise<any> => {
+  console.log(`[limit:${limit}] Fetch from ${url}: `);
+  if(limit < 0){
+      console.warn(`Try to fetch '${url}' over limit`);
+      return;
+  }
+  try {
+      return axios.get(url, axiosConfig);
+  } catch (e) {
+      console.warn(`Something wrong: ${e}`);
+      await setTimeoutPromise(delayTime * 1000);
+      return fetchRetry(url, delayTime, limit - 1);
+  }
+}
+
+const initFiles = [];
+
 const ContributorsDetail = ({ username }: { username: string }) => {
 
-  const [commitList, setCommitList] = useState([]);
+  const [files, setFiles] = useState(initFiles);
   
   useEffect(() => {
     const fetch = async () => {
-      const data = (await axios.get(`https://api.github.com/repos/dotnetthailand/dotnetthailand.github.io/commits?author=${username}`, axiosConfig)).data;
-      const commitListTmp = data.map(commit => ({
-        date: commit?.commit?.author?.date,
-        message: commit?.commit?.message,
-        url: commit?.url,
-      }))
-      setCommitList(commitListTmp);
+      const data = (await fetchRetry(`https://api.github.com/repos/dotnetthailand/dotnetthailand.github.io/commits?author=${username}`,3 , 3)).data;
+      const commitListTmp = data.map(commitData => ({
+        date: commitData?.commit?.author?.date,
+        message: commitData?.commit?.message,
+        url: commitData?.url,
+      }));
+
+      const fileDictionary:Record<string, IFile> = {}; 
+
+      // for (const commitData of commitListTmp) {
+        const commitData = commitListTmp[0];
+        console.log(commitData);
+        const commitFiles = (await fetchRetry(commitData?.url, 3, 3)).data;
+        console.log(commitFiles.files);
+        for (const commitFile of commitFiles.files) {
+
+          console.log(commitFile);
+
+          const uniqueFile = fileDictionary[commitFile.filename];
+          if (uniqueFile) {
+            fileDictionary[commitFile.filename].commitCount ++;
+          } else {
+            fileDictionary[commitFile.filename] = { commitCount: 1, filename: commitFile.filename }
+          }
+        }
+        
+        console.log(fileDictionary)
+        
+        const serializedFileList = Object.entries(fileDictionary).map(([, file]) => file);
+        setFiles(serializedFileList);
+        console.log(serializedFileList)
+        // Create a unique file path
+
+        // update state
+
+      // }
     }
 
     fetch();
@@ -28,8 +90,8 @@ const ContributorsDetail = ({ username }: { username: string }) => {
 
   return (
     <div>
-      {commitList.map(commit => (
-        <div key={commit.url}>{commit.date}, {commit.message}</div>
+      {files.map((file: IFile) => (
+        <div key={file.filename}>{file.filename} ({file.commitCount})</div>
       ))}
     </div>
   )
