2bc390c463d222cfb648f8d23f20d1accdb251ba
Thada Wangthammang <thada.wth@gmail.com>
2021-08-10 00:00:41 +0700

    feat: filter only content to show in contributors

--- src/components/MdxComponents/ContributorsDetail.tsx
+++ src/components/MdxComponents/ContributorsDetail.tsx
@@ -8,12 +8,6 @@ const axiosConfig = {
   }
 }
 
-interface ICommit {
-  date: Date;
-  message: string;
-  url: string;
-}
-
 interface IFile {
   commitCount: number;
   filename: string;
@@ -39,14 +33,20 @@ const fetchRetry = async (url: string, delayTime: number, limit: number): Promis
   }
 }
 
+const isContent = (filename: string) => {
+  return /^content/gm.test(filename);
+}
+
 const initFiles = [];
 
 const ContributorsDetail = ({ username }: { username: string }) => {
 
   const [files, setFiles] = useState(initFiles);
+  const [loading, setLoading] = useState(false);
   
   useEffect(() => {
     const fetch = async () => {
+      setLoading(true);
       const data = (await fetchRetry(`https://api.github.com/repos/dotnetthailand/dotnetthailand.github.io/commits?author=${username}`,3 , 3)).data;
       const commitListTmp = data.map(commitData => ({
         date: commitData?.commit?.author?.date,
@@ -56,14 +56,15 @@ const ContributorsDetail = ({ username }: { username: string }) => {
 
       const fileDictionary:Record<string, IFile> = {}; 
 
-      // for (const commitData of commitListTmp) {
-        const commitData = commitListTmp[0];
+      for (const commitData of commitListTmp) {
+        // const commitData = commitListTmp[0];
         console.log(commitData);
         const commitFiles = (await fetchRetry(commitData?.url, 3, 3)).data;
         console.log(commitFiles.files);
         for (const commitFile of commitFiles.files) {
 
           console.log(commitFile);
+          if(!isContent(commitFile.filename)) continue;
 
           const uniqueFile = fileDictionary[commitFile.filename];
           if (uniqueFile) {
@@ -75,14 +76,18 @@ const ContributorsDetail = ({ username }: { username: string }) => {
         
         console.log(fileDictionary)
         
-        const serializedFileList = Object.entries(fileDictionary).map(([, file]) => file);
+        const serializedFileList = Object.entries(fileDictionary)
+          .map(([, file]) => file)
+          .sort((a, b) => (a.commitCount < b.commitCount) ? 1 : -1);
+
         setFiles(serializedFileList);
         console.log(serializedFileList)
         // Create a unique file path
 
         // update state
 
-      // }
+      }
+      setLoading(false);
     }
 
     fetch();
@@ -90,6 +95,7 @@ const ContributorsDetail = ({ username }: { username: string }) => {
 
   return (
     <div>
+      <div>{loading && `Loading....... `}</div>
       {files.map((file: IFile) => (
         <div key={file.filename}>{file.filename} ({file.commitCount})</div>
       ))}
