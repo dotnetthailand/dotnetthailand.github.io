22ce653d7273a99c988614120da8fe7aac6ea577
Thada Wangthammang <thada.wth@gmail.com>
2021-08-11 00:31:17 +0700

    feat: add ui for contributor commit

--- src/components/MdxComponents/ContributorsDetail.tsx
+++ src/components/MdxComponents/ContributorsDetail.tsx
@@ -7,10 +7,10 @@ import { DefaultButton } from '@fluentui/react/lib/Button';
 import styled from '@emotion/styled';
 import { Persona, PersonaSize } from '@fluentui/react/lib/Persona';
 import { ProgressIndicator } from '@fluentui/react/lib/ProgressIndicator';
+import { DetailsList, DetailsListLayoutMode, SelectionMode } from '@fluentui/react/lib/DetailsList';
 
 // Feature Toggle
 const feature = {
-  ignoreMergePullRequest: true,
   ignoreFilesExtension: true,
 }
 
@@ -18,11 +18,16 @@ const GithubProfile = styled.div`
   margin: 40px 0px;
 `;
 
+const RoleHeader = styled.h3`
+  margin-top: 40px;
+  margin-bottom: 15px;
+`;
 
 const style = theme => css`
 
   width: 100%;
   margin-top: 40px;
+  margin-bottom: 40px;
 
   .profile {
     & > img {
@@ -30,6 +35,14 @@ const style = theme => css`
       width: 100px;
       height: 100px;
     }
+  }
+
+  a, a:active, a:visited {
+    color: ${theme.colors.primary}
+  }
+
+  a:hover {
+    color: ${theme.colors.primaryDark}
   }
 
 `;
@@ -50,11 +63,13 @@ const ignoreFilesExtension: RegExp[] = [
 ];
 
 interface IFile {
+  key: string;
   commitCount: number;
   filename: string;
   contentUrl: string;
-  title?: string;
+  title: string;
   isContent: boolean;
+  logUrl: string;
 }
 
 interface IGithubUser {
@@ -67,8 +82,15 @@ const setTimeoutPromise = (timeout: number) => new Promise(resolve => {
   setTimeout(resolve, timeout);
 });
 
-const getUserRole = () => {
-  return "Content Editor, Maintainer, Developer"
+const getUserRole = (filesLength: number, contentFilesLength: number, mergePrFilesLength: number) => {
+  let roles = [];
+  if(contentFilesLength > 0)
+    roles.push('Content Editor');
+  if(filesLength > 0)
+    roles.push('Developer');
+  if(mergePrFilesLength > 0)
+    roles.push('Maintainer');
+  return roles.join(', ');
 }
 
 
@@ -114,12 +136,24 @@ const getTitle = async (rssData: string, path: string) => {
   return;
 }
 
+const columns = [
+  { key: 'title', name: 'File', fieldName: 'title', minWidth: 100, maxWidth: 700, isResizable: true,
+    onRender: (item: IFile) => {
+        return <a href={item.contentUrl} target='_blank' rel='noreferrer'>{item.title}</a>;
+    }},
+  { key: 'logUrl', name: 'Log', fieldName: 'logUrl', minWidth: 100, maxWidth: 200, isResizable: true, 
+    onRender: (item: IFile) => {
+      return <a href={item.logUrl} target='_blank' rel='noreferrer'>See more</a>;
+  },},
+];
+
 const initFiles = [];
 
 const ContributorsDetail = ({ username }: { username: string }) => {
   const theme = useTheme();
   const [files, setFiles] = useState(initFiles);
   const [contentFiles, setContentFiles] = useState(initFiles);
+  const [mergePrFiles, setMergePrFiles] = useState(initFiles);
   const [loading, setLoading] = useState(false);
   const [githubUser, setGithubUser] = useState({} as IGithubUser);
   const [percentComplete, setPercentComplete] = useState(0);
@@ -130,33 +164,59 @@ const ContributorsDetail = ({ username }: { username: string }) => {
       const rssData = (await fetchRetry(`https://www.dotnetthailand.com/rss.xml`, 3, 3)).data;
       const data = (await fetchRetry(`https://api.github.com/repos/${repo}/commits?author=${username}`, 3, 3)).data;
       const commitListTmp = data.map(commitData => ({
+        sha: commitData?.sha,
         date: commitData?.commit?.author?.date,
         message: commitData?.commit?.message,
         url: commitData?.url,
+        html_url: commitData?.html_url,
       }));
 
       const fileDictionary: Record<string, IFile> = {};
       const contentFileDictionary: Record<string, IFile> = {};
+      const mergePrFileDictionary: Record<string, IFile> = {};
+
       for (let i = 0; i < commitListTmp.length; i++) {
         const commitData = commitListTmp[i];
+        console.log(isMergePullRequest(commitData.message), commitData.message)
+        if (isMergePullRequest(commitData.message)) {
+          const newFile: IFile = {
+            key: commitData.sha,
+            commitCount: 1,
+            filename: '',
+            contentUrl: '',
+            title: commitData.message,
+            isContent: false,
+            logUrl: commitData.html_url,
+          }
+          mergePrFileDictionary[commitData.sha] = newFile;
+          console.log(mergePrFileDictionary)
+          const serializedMergePrFileList = Object.entries(mergePrFileDictionary)
+            .map(([, file]) => file);
+          setMergePrFiles(serializedMergePrFileList);
+          setPercentComplete((i+1)/commitListTmp.length);
+          continue;
+        } 
+
         const commitFiles = (await fetchRetry(commitData?.url, 3, 3)).data;
         for (const commitFile of commitFiles.files) {
 
           if (isIgnoreFile(commitFile.filename) && feature.ignoreFilesExtension) continue;
-          if (isMergePullRequest(commitData.message) && feature.ignoreMergePullRequest) continue;
+          // if (isMergePullRequest(commitData.message) && feature.ignoreMergePullRequest) continue;
 
           const contentUrl = commitFile.filename.replace(/^content/, '').replace(/\..+$/, '');
           const absolutePath = `https://www.dotnetthailand.com/${contentUrl.replace(/^\//, '')}`;
           const title = await getTitle(rssData, contentUrl);
           const newFile: IFile = {
+            key: commitFile.filename,
             commitCount: 1,
             filename: commitFile.filename,
             contentUrl: title ? absolutePath : commitFile.blob_url,
-            title,
+            title: title || commitFile.filename,
             isContent: isContent(commitFile.filename),
+            logUrl: `https://github.com/${repo}/commits/main/${commitFile.filename}`
           }
-
-          if (isContent(commitFile.filename)) {
+          
+          if(isContent(commitFile.filename)) {
             if (contentFileDictionary[commitFile.filename])
               contentFileDictionary[commitFile.filename].commitCount++;
             else
@@ -169,7 +229,6 @@ const ContributorsDetail = ({ username }: { username: string }) => {
           }
         }
 
-
         const serializedFileList = Object.entries(fileDictionary)
           .map(([, file]) => file)
           .sort((a, b) => (a.title && !b.title) ? 1 : -1)
@@ -181,9 +240,10 @@ const ContributorsDetail = ({ username }: { username: string }) => {
 
         setFiles(serializedFileList);
         setContentFiles(serializedContentFileList);
-        setPercentComplete(Math.ceil((i+1)/commitListTmp.length));
+        setPercentComplete((i+1)/commitListTmp.length);
+        
       }
-
+      setPercentComplete(1);
       setLoading(false);
     }
 
@@ -210,7 +270,11 @@ const ContributorsDetail = ({ username }: { username: string }) => {
             imageUrl={githubUser.avatarUrl}
             imageInitials={githubUser.name}
             text={githubUser.name}
-            secondaryText={getUserRole()}
+            secondaryText={getUserRole(
+              files.length,
+              contentFiles.length,
+              mergePrFiles.length,
+            )}
             tertiaryText={`@${username}`}
             size={PersonaSize.size72}
             hidePersonaDetails={false}
@@ -220,24 +284,52 @@ const ContributorsDetail = ({ username }: { username: string }) => {
       </GithubProfile>
 
       <h4>{loading && <ProgressIndicator label="Extracting git commit history" percentComplete={percentComplete} />}</h4>
-      <h2>Content</h2>
-      {contentFiles.map((file: IFile) => (
+      
+      {/* {contentFiles.map((file: IFile) => (
         <div key={file.filename}>
           {file.title ? <a href={file.contentUrl} target='_blank' rel='noreferrer'>{file.title}</a> : <a href={file.contentUrl} target='_blank' rel='noreferrer'>{file.filename}</a>}
           ({file.commitCount})
           <a href={`https://github.com/${repo}/commits/main/${file.filename}`} target='_blank' rel='noreferrer'>Commit History</a>
         </div>
-      ))}
-      <h2>Web Development</h2>
-      <div>
-        {files.map((file: IFile) => (
-          <div key={file.filename}>
-            {file.title ? <a href={file.contentUrl} target='_blank' rel='noreferrer'>{file.title}</a> : <a href={file.contentUrl} target='_blank' rel='noreferrer'>{file.filename}</a>}
-            ({file.commitCount})
-            <a href={`https://github.com/${repo}/commits/main/${file.filename}`} target='_blank' rel='noreferrer'>Commit History</a>
-          </div>
-        ))}
-      </div>
+      ))} */}
+      {contentFiles.length > 0 && 
+        <>
+          <RoleHeader>Content</RoleHeader>
+          <DetailsList
+            items={contentFiles}
+            columns={columns}
+            layoutMode={DetailsListLayoutMode.justified}
+            selectionPreservedOnEmptyClick={true}
+            selectionMode={SelectionMode.none}
+          />
+        </>
+      }
+      
+      {files.length > 0 && 
+        <>
+          <RoleHeader>Code</RoleHeader>
+          <DetailsList
+            items={files}
+            columns={columns}
+            layoutMode={DetailsListLayoutMode.justified}
+            selectionPreservedOnEmptyClick={true}
+            selectionMode={SelectionMode.none}
+          />
+        </>
+      }
+
+      {mergePrFiles.length > 0 && 
+        <>
+          <RoleHeader>Review & Maintenance</RoleHeader>
+          <DetailsList
+            items={mergePrFiles}
+            columns={columns}
+            layoutMode={DetailsListLayoutMode.justified}
+            selectionPreservedOnEmptyClick={true}
+            selectionMode={SelectionMode.none}
+          />
+        </>
+      }
     </div>
   )
 };
