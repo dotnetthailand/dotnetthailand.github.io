34fff9823afb7de3d47ed570344ff1f8bb7e4cb4
aaronamm <theeranitp@gmail.com>
2021-08-20 16:13:16 +0700

    Pull onServiceWorkerUpdateReady in gatsby-browser.js back because it helps updating new content

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
 import 'loading-css';
+
+// Since we use WPA, we can't remove this code black.
+// This is because Fluent UI font requires PWA to work properly.
+// REF https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/#onServiceWorkerUpdateReady
+export const onServiceWorkerUpdateReady = () => {
+  // Auto reload to display the latest version, when service worker update ready.
+  window.location.reload();
+};
