672af8ece2c5b2d9cc4f824569c27702bba182fa
aaronamm <theeranitp@gmail.com>
2021-12-25 00:00:31 +0700

    Transparent snowflake

--- src/components/FallingSnow.tsx
+++ src/components/FallingSnow.tsx
@@ -3,11 +3,11 @@ import { useEffect } from 'react';
 const isBrowser = typeof window !== "undefined"
 
 function getBrowserHeight() {
-    if (!isBrowser) {
-        return 0;
-    }
+  if (!isBrowser) {
+    return 0;
+  }
 
-    return window.innerHeight;
+  return window.innerHeight;
 }
 
 const MAX_SPEED = 10;
@@ -17,14 +17,15 @@ const MIN_SPEED = 5;
 const MAX_PATH_WIDTH = 30;
 const MIN_PATH_WIDTH = 20;
 
+// period
 const MAX_PATH_HEIGHT = getBrowserHeight();
 const MIN_PATH_HEIGHT = 300;
 
 // Max and min size of snowflake
-const MAX_SIZE = 16;
-const MIN_SIZE = 8;
+const MAX_WIDTH = 16;
+const MIN_WIDTH = 8;
 
-const NUMBER_OF_SNOW_FLAKES = 50;
+const NUMBER_OF_SNOW_FLAKES = 30;
 
 /**
  * Returns a random number between min and max.
@@ -33,14 +34,14 @@ const NUMBER_OF_SNOW_FLAKES = 50;
  * @return random number between min (included) and max (not included):
  */
 function random(min: number, max: number) {
-    // https://stackoverflow.com/a/24152886/1872200
-    return Math.random() * (max - min) + min;
+  // https://stackoverflow.com/a/24152886/1872200
+  return Math.random() * (max - min) + min;
 }
 
 // Equation x = h * sin((2PI/w)y)
 // Input y value and return x value
 function sinGraph(yValue: number, adjustedWaveHeight: number, adjustedWaveLength: number) {
-    return adjustedWaveHeight * Math.sin(((2 * Math.PI) / adjustedWaveLength) * yValue);
+  return adjustedWaveHeight * Math.sin(((2 * Math.PI) / adjustedWaveLength) * yValue);
 }
 
 /**
@@ -48,96 +49,96 @@ function sinGraph(yValue: number, adjustedWaveHeight: number, adjustedWaveLength
  * @param Image imageObj The image object to be used as a snow flake
  */
 class SnowFlake {
-    private interval: NodeJS.Timer = null;
+  private interval: NodeJS.Timer = null;
 
-    private x: number;
-    private y: number;
+  private x: number;
+  private y: number;
 
-    private startX: number;
-    private speed: number;
+  private startX: number;
+  private speed: number;
 
-    private pathWidth: number;
-    private pathHeight: number;
+  private pathWidth: number;
+  private pathHeight: number;
 
-    constructor(public imageElement: HTMLImageElement) {
-        this.imageElement.style.position = 'fixed';
-        this.imageElement.style.zIndex = '1000';
-        this.imageElement.src = '/assets/snowflake.png';
-        this.reset();
+  constructor(public imageElement: HTMLImageElement) {
+    this.imageElement.style.position = 'fixed';
+    this.imageElement.style.zIndex = '1000';
+    this.imageElement.src = '/assets/snowflake.png';
+    this.reset();
+  }
+
+  /**
+   * Starts the animation for this object. To stop the animation call stopAnimation.
+   */
+  startAnimation() {
+    this.interval = setInterval(() => this.animation(sinGraph), 100);
+  }
+
+  /**
+   * Stops the animation for this object. To start the animation again call startAnimation.
+   */
+  stopAnimation() {
+    clearInterval(this.interval);
+  }
+
+  // Starts an infinite animation loop using the given function to move
+  // and change the size of the given object.
+  private animation(sinGraph: (yValue: number, adjustedWaveHeight: number, adjustedWaveLength: number) => number) {
+    this.y += this.speed; // Keep increasing y value with small amount or big amount of speed value.
+
+    if (this.pathWidth !== 0 && this.pathHeight !== 0) {
+      this.x = sinGraph(this.y, this.pathWidth, this.pathHeight) + this.startX; // shift on x axis with startX value
     }
 
-    /**
-     * Starts the animation for this object. To stop the animation call stopAnimation.
-     */
-    startAnimation() {
-        this.interval = setInterval(() => this.animation(sinGraph), 100);
+    // Check if snow flake y value is out of the frame
+    if (this.y >= MAX_PATH_HEIGHT) {
+      this.reset();
+    } else {
+      this.imageElement.style.top = `${this.y}px`;
     }
 
-    /**
-     * Stops the animation for this object. To start the animation again call startAnimation.
-     */
-    stopAnimation() {
-        clearInterval(this.interval);
+    if (this.x <= window.innerWidth) {
+      this.imageElement.style.left = `${this.x}px`;
     }
+  }
 
-    // Starts an infinite animation loop using the given function to move
-    // and change the size of the given object.
-    private animation(sinGraph: (yValue: number, adjustedWaveHeight: number, adjustedWaveLength: number) => number) {
-        this.y += this.speed; // Keep increasing y value with small amount or big amount of speed value.
+  /**
+   * Resets teh status of the object with new random values
+   */
+  private reset() {
 
-        if (this.pathWidth !== 0 && this.pathHeight !== 0) {
-            this.x = sinGraph(this.y, this.pathWidth, this.pathHeight) + this.startX; // shift on x axis with startX value
-        }
+    this.startX = random(0, window.innerWidth);
+    // * -1 To start above browser window, top with negative value.
+    const startY = random(0, MAX_PATH_HEIGHT) * -1;
+    this.x = this.startX;
+    this.y = startY;
 
-        // Check if snow flake y value is out of the frame
-        if (this.y >= MAX_PATH_HEIGHT) {
-            this.reset();
-        } else {
-            this.imageElement.style.top = `${this.y}px`;
-        }
+    this.speed = random(MIN_SPEED, MAX_SPEED);
+    this.pathWidth = random(MIN_PATH_WIDTH, MAX_PATH_WIDTH);
+    this.pathHeight = random(MIN_PATH_HEIGHT, MAX_PATH_HEIGHT);
 
-        if (this.x <= window.innerWidth) {
-            this.imageElement.style.left = `${this.x}px`;
-        }
-    }
-
-    /**
-     * Resets teh status of the object with new random values
-     */
-    private reset() {
-
-        this.startX = random(0, window.innerWidth);
-        // * -1 To start above browser window, top with negative value.
-        const startY = random(0, MAX_PATH_HEIGHT) * -1;
-        this.x = this.startX;
-        this.y = startY;
-
-        this.speed = random(MIN_SPEED, MAX_SPEED);
-        this.pathWidth = random(MIN_PATH_WIDTH, MAX_PATH_WIDTH);
-        this.pathHeight = random(MIN_PATH_HEIGHT, MAX_PATH_HEIGHT);
-
-        const size = random(MIN_SIZE, MAX_SIZE);
-        this.imageElement.style.width = `${size}px`;
-        this.imageElement.style.height = `${size}px`;
-    }
+    const size = random(MIN_WIDTH, MAX_WIDTH);
+    this.imageElement.style.width = `${size}px`;
+    this.imageElement.style.height = 'auto';
+  }
 }
 
 export default function FallingSnow() {
-    useEffect(() => {
+  useEffect(() => {
 
-        // Create some snow flakes.
-        const snowFlakes = [...Array(NUMBER_OF_SNOW_FLAKES).keys()].map(() => {
-            const imageElement = document.createElement('img');
-            return new SnowFlake(imageElement);
-        });
+    // Create some snow flakes.
+    const snowFlakes = [...Array(NUMBER_OF_SNOW_FLAKES).keys()].map(() => {
+      const imageElement = document.createElement('img');
+      return new SnowFlake(imageElement);
+    });
 
-        const bodyElement = document.getElementsByTagName('body')[0];
-        snowFlakes.forEach((snowFlake) => {
-            bodyElement.appendChild(snowFlake.imageElement);
-            snowFlake.startAnimation();
-        });
-    }, []);
+    const bodyElement = document.getElementsByTagName('body')[0];
+    snowFlakes.forEach((snowFlake) => {
+      bodyElement.appendChild(snowFlake.imageElement);
+      snowFlake.startAnimation();
+    });
+  }, []);
 
-    // https://stackoverflow.com/a/22105413/1872200
-    return null;
+  // https://stackoverflow.com/a/22105413/1872200
+  return null;
 }
